d6bc8c7975934996d59a4805147a38f18a9db306
B00131319 Maksymilian Krawczyk <B00131319@mytudublin.ie>
2024-02-07 12:50:46 +0000

    perfect number fixed

--- perfectNumber.py
+++ perfectNumber.py
@@ -6,14 +6,14 @@
 #number itself. For example the divisors of 28 are 1,2,4,7,14 which added together gives 28
 #write a function below called perfectNumber(x) which checks to see if x is a perfect number
 #The function should return True if the number is perfect and False if it is not
-
-from divisors import divisors
 from divisors import divisors
 
 def perfectNumber(x):
     result = False
     
-    if sum(divisors(x)) == x:
+    if sum(divisors(x)) - x == x:
         result = True
     
     return result
+print (perfectNumber(10))
+
